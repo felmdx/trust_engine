0cc7b50e96755e97d2708274c2b57cbb5727b66b
felmdx <felmds.h@gmail.com>
2026-03-01 19:58:24 -0300

    Estrutura inicial com Maven

---
+++ src/main/java/com/trustengine/trust_engine/TrustEngineApplication.java
@@ -0,0 +1,13 @@
+package com.trustengine.trust_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustEngineApplication.class, args);
+	}
+
+}
