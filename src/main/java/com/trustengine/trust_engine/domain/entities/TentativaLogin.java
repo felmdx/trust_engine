b3ff745dbcc3e3266b23c526469e28b452233b5a
felmdx <felmds.h@gmail.com>
2026-03-02 20:34:41 -0300

    Novas entidades adicionadas #0203

---
+++ src/main/java/com/trustengine/trust_engine/domain/entities/TentativaLogin.java
@@ -0,0 +1,45 @@
+package com.trustengine.trust_engine.domain.entities;
+
+import lombok.Getter;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.trustengine.trust_engine.domain.enums.RiscoCalculado;
+
+@Getter
+public class TentativaLogin {
+    private final UUID id;
+    private final UUID usuarioId;
+    private final String ipOrigem;
+    private final String userAgent;
+    private final LocalDateTime timestamp;
+    private final boolean senhaCorreta;
+    private final RiscoCalculado riscoCalculado;
+
+    public TentativaLogin(UUID usuarioId, String ipOrigem, String userAgent, boolean senhaCorreta, RiscoCalculado riscoCalculado) {
+        validate(usuarioId, ipOrigem);
+        this.id = UUID.randomUUID();
+        this.usuarioId = usuarioId;
+        this.ipOrigem = ipOrigem;
+        this.userAgent = userAgent;
+        this.senhaCorreta = senhaCorreta;
+        this.riscoCalculado = riscoCalculado;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public TentativaLogin(UUID id, UUID usuarioId, String ipOrigem, String userAgent, LocalDateTime timestamp, boolean senhaCorreta, RiscoCalculado riscoCalculado) {
+        this.id = id;
+        this.usuarioId = usuarioId;
+        this.ipOrigem = ipOrigem;
+        this.userAgent = userAgent;
+        this.timestamp = timestamp;
+        this.senhaCorreta = senhaCorreta;
+        this.riscoCalculado = riscoCalculado;
+    }
+
+    public void validate(UUID usuarioId, String ipOrigem){
+        if (usuarioId == null || ipOrigem == null || ipOrigem.isBlank()) {
+            throw new IllegalArgumentException("Usuário e IP de origem são obrigatórios para registrar a tentativa.");
+        }
+    }
+}
